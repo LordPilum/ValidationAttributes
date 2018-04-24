9d2b42fceb5712b7b61d3f250fe3b3027b4e6b2a
Jan Kjetil Myklebust <jan.kjetil.myklebust@enoro.com>
2018-04-24 08:16:53 +0200

    Removed superfluous code.

--- ValidationAttributes/CustomValidationAttribute/Validator.cs
+++ ValidationAttributes/CustomValidationAttribute/Validator.cs
@@ -116,14 +116,6 @@ namespace ValidationAttributes.CustomValidationAttribute
                 if (src == null) throw new ArgumentException("Value cannot be null.", nameof(src));
                 if (propName == null) throw new ArgumentException("Value cannot be null.", nameof(propName));
 
-                if (propName.Contains("."))
-                {
-                    var temp = propName.Split(new [] {'.'}, 2);
-                    src = GetPropValue(src, temp[0]);
-                    propName = temp[1];
-                    continue;
-                }
-
                 var prop = src.GetType().GetProperty(propName);
                 return prop?.GetValue(src, null);
             }
